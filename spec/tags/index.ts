1659467b73edcbeddba40513c30249157a59a964
Wladimir Mangelardo <wmangelardo@gmail.com>
2019-07-22 18:18:45 -0300

    Added tag for Subscription endpoint to show the schema used for events since callback feature do not work on ReDoc

--- spec/tags/index.ts
+++ spec/tags/index.ts
@@ -2,14 +2,18 @@ import { TagObject } from 'openapi3-ts';
 import { rawLoad } from '../../utils/raw-load';
 
 const whatsappDescription = rawLoad(__dirname, './whatsapp.md');
-const facbookDescription = rawLoad(__dirname, './facebook.md');
+const facebookDescription = rawLoad(__dirname, './facebook.md');
+const subscriptionsDescription = rawLoad(__dirname, './subscriptions.md');
 
 const tags: TagObject[] = [{
   name: 'WhatsApp',
   description: whatsappDescription,
 }, {
   name: 'Facebook',
-  description: facbookDescription,
+  description: facebookDescription,
+}, {
+  name: 'Subscriptions',
+  description: subscriptionsDescription,
 }];
 
 export default tags;
