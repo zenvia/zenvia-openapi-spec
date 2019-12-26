862fcc842d9fbdf6f3af938a80ae2e6b19a75de4
Jhonnanthn Balsas <jhonnanthn.balsas@zenvia.com>
2019-12-26 14:45:14 -0300

    Putting a description in visitor object.

--- spec/components/schemas/message/visitor.ts
+++ spec/components/schemas/message/visitor.ts
@@ -2,18 +2,23 @@ import { SchemaObject } from 'openapi3-ts';
 import { createComponentRef } from '../../../../utils/ref';
 
 const visitor: SchemaObject = {
+  title: 'Visitor Object',
+  description: 'Indicate the name of the visitor who sent the message',
   type: 'object',
   properties: {
     name: {
       title: 'Name',
+      description: 'Full name of the visitor',
       type: 'string',
     },
     firstName: {
       title: 'First name',
+      description: 'First name of the visitor',
       type: 'string',
     },
     lastName: {
       title: 'Last name',
+      description: 'Last name of the visitor',
       type: 'string',
     },
   },
