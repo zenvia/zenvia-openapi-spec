d569c3b6b7276299e8d2160c0ef903cbda334616
Henrique Magarotto <magarotto@gmail.com>
2020-04-16 22:52:51 -0300

    Added prossible template category values

--- spec/components/schemas/templates/template-category.ts
+++ spec/components/schemas/templates/template-category.ts
@@ -6,6 +6,19 @@ const category: SchemaObject = {
   title: 'Category',
   description: 'Category of use for this template',
   type: 'string',
+  enum: [
+    'ACCOUNT_UPDATE',
+    'PAYMENT_UPDATE',
+    'PERSONAL_FINANCE_UPDATE',
+    'SHIPPING_UPDATE',
+    'RESERVATION_UPDATE',
+    'ISSUE_RESOLUTION',
+    'APPOINTMENT_UPDATE',
+    'TRANSPORTATION_UPDATE',
+    'TICKET_UPDATE',
+    'ALERT_UPDATE',
+    'AUTO_REPLY',
+  ],
 };
 
 export const ref = createComponentRef(__filename);
