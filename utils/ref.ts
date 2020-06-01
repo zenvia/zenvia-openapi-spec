ddf7fa1afcc1859fdde3eec1a8862d371dac125e
Henrique Magarotto <magarotto@gmail.com>
2020-06-01 18:39:44 -0300

    Fixing OpaneAPI components ref string creation

--- utils/ref.ts
+++ utils/ref.ts
@@ -1,7 +1,7 @@
 import { basename } from 'path';
 
 export function createComponentRef(path: string) {
-  const componentPath = path.split(/components\//)[1];
+  const componentPath = path.split(/\/spec\/components\//)[1];
   const parts = componentPath.split(/\//);
   const type = parts.splice(0, 1)[0];
   const fileName = basename(parts.pop(), '.ts');
