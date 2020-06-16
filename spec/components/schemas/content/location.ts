8796a1fe1fd4b8828774cc4e2fcc56c9a5ee3f5d
joaoneves <bonoddr@gmail.com>
2020-06-15 23:13:15 -0300

    Provide sample content for location

--- spec/components/schemas/content/location.ts
+++ spec/components/schemas/content/location.ts
@@ -11,23 +11,23 @@ const location: SchemaObject = {
     properties: {
       longitude: {
         type: 'number',
-        description: 'Longitude of the location',
+        description: 'Longitude of the location. Example: -122.425332',
       },
       latitude: {
         type: 'number',
-        description: 'Latitude of the location',
+        description: 'Latitude of the location. Example: 37.758056',
       },
       name: {
         type: 'string',
-        description: 'Name of the location',
+        description: 'Name of the location. Example: \'Home\'',
       },
       address: {
         type: 'string',
-        description: 'Address of the location. Only displayed if name is present.',
+        description: 'Address of the location. Only displayed if name is present. Example: \'Home Address\'',
       },
       url: {
         type: 'string',
-        description: 'URL for the website where the location information was downloaded. _(WhatsApp restriction: available only on IN direction)_',
+        description: 'URL for the website where the location information was downloaded _(WhatsApp restriction: available only on IN direction)_. Example: <a href="https://www.zenvia.com" target="_blank">\'https://www.zenvia.com\'</a>.',
       },
     },
     required: [
