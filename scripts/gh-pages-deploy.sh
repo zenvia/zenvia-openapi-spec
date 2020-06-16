2c3005371e31cbbfb595a1b45e260c9769a8fe45
Guilherme Chinko <guilherme.chinko@zenvia.com>
2020-06-16 19:09:35 -0300

    Fixed path for github project

--- scripts/gh-pages-deploy.sh
+++ scripts/gh-pages-deploy.sh
@@ -1,6 +1,6 @@
-git clone --branch gh-pages "https://${GH_TOKEN}@github.com/hmagarotto/zenvia-openapi-spec.git"  gh-pages
+git clone --branch gh-pages "https://${GH_TOKEN}@github.com/zenvia/zenvia-openapi-spec.git"  gh-pages
 cp -RT web_deploy gh-pages
 cd gh-pages
 git add .
 git commit -m'Deploy'
-git push "https://${GH_TOKEN}@github.com/hmagarotto/zenvia-openapi-spec.git"
+git push "https://${GH_TOKEN}@github.com/zenvia/zenvia-openapi-spec.git"
