a424647607698e7b4b6082cd34cea5b725da35d6
joaoneves <bonoddr@gmail.com>
2020-06-22 18:20:56 -0300

    Removing content for specific channels

--- spec/tags/index.ts
+++ spec/tags/index.ts
@@ -2,8 +2,6 @@ import { TagObject } from 'openapi3-ts';
 import { rawLoad } from '../../utils/raw-load';
 
 const contentTypesDescription = rawLoad(__dirname, './message-content-types.md');
-const whatsappDescription = rawLoad(__dirname, './whatsapp.md');
-const facebookDescription = rawLoad(__dirname, './facebook.md');
 const subscriptionsDescription = rawLoad(__dirname, './subscriptions.md');
 const templatesDescription = rawLoad(__dirname, './templates.md');
 const messageReportDescription = rawLoad(__dirname, './message-report.md');
@@ -12,12 +10,6 @@ const flowReportDescription = rawLoad(__dirname, './flow-report.md');
 const tags: TagObject[] = [{
   name: 'Messaging',
   description: contentTypesDescription,
-}, {
-  name: 'WhatsApp',
-  description: whatsappDescription,
-}, {
-  name: 'Facebook',
-  description: facebookDescription,
 }, {
   name: 'Subscriptions',
   description: subscriptionsDescription,
