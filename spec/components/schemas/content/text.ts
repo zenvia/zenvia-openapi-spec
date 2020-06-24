e00b9877685fc53fba3d499fa7e8f44d51ee195c
joaoneves <bonoddr@gmail.com>
2020-06-23 23:11:23 -0300

    Examples for text

--- spec/components/schemas/content/text.ts
+++ spec/components/schemas/content/text.ts
@@ -12,6 +12,7 @@ const text: SchemaObject = {
       text: {
         description: 'Text to be sent',
         type: 'string',
+        example: 'This is a text',
       },
       payload: {
         description: 'Payload of selected button',
