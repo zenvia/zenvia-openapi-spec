1d703aa1bb2701d4330ab132523c37e8d5bdec02
joaoneves <bonoddr@gmail.com>
2020-06-24 00:04:14 -0300

    Examples for template

--- spec/components/schemas/content/template.ts
+++ spec/components/schemas/content/template.ts
@@ -12,6 +12,7 @@ const template: SchemaObject = {
       templateId: {
         type: 'string',
         description: 'The template identifier. Click <a href="https://app.zenvia.com/home/templates" target="_blank">here</a> to go to the template page.',
+        example: 'c2c3e42d-6fb7-4ad6-897a-dd7613469f58',
       },
       fields: {
         type: 'object',
@@ -20,6 +21,10 @@ const template: SchemaObject = {
           field: {
             type: 'string',
             description: 'Message that will be replaced on field',
+            example: {
+              user: 'John Smith',
+              protocol: '34534252',
+            },
           },
         },
       },
