6c78239700d36b0c1266f7c55b0362793580bfbf
brunofprusch <bruno.prusch@zenvia.com>
2020-09-10 13:35:22 -0300

    Add field to inform the standard answer

--- spec/components/schemas/batches/base.ts
+++ spec/components/schemas/batches/base.ts
@@ -23,6 +23,11 @@ const base: SchemaObject = {
       description: 'Message channel',
       type: 'string',
     },
+    starndardAnswer: {
+      title: 'Standard Answer',
+      description: 'Standard answer to be sent if recipient to reply the message sent',
+      type: 'string',
+    },
   },
   required: [
     'name',
