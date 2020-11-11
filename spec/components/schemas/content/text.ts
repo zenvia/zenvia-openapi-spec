f39cb6675fc71139af49d8db9b0b5fdc91b3433f
Rafael Souza <rafael@rafael.org>
2020-11-11 16:27:34 -0300

    Fixing encodingMethod definition

--- spec/components/schemas/content/text.ts
+++ spec/components/schemas/content/text.ts
@@ -34,7 +34,6 @@ const text: SchemaObject = {
         example: 'true',
       },
       encodingMethod: {
-        name: 'encodingMethod',
         type: 'string',
         enum: [
           'AUTO',
