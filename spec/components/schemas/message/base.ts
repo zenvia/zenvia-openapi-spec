26dd81692e8573cdd5cf8d5498bfe41c23fdf2d2
felipedalcarobo <felipedalcarobo@ymail.com>
2021-04-01 10:23:04 -0300

    Revert "merge"

--- spec/components/schemas/message/base.ts
+++ spec/components/schemas/message/base.ts
@@ -51,12 +51,6 @@ const base: SchemaObject = {
       },
       minItems: 1,
     },
-    timestamp: {
-      title: 'Message timestamp',
-      type: 'string',
-      format: 'date-time',
-      readOnly: true,
-    },
   },
   required: [
     'from',
