feac15437bd406aeb1903ad8a73bae678bd90e91
Rafael Souza <rafael@rafael.org>
2021-07-06 14:33:18 -0300

    Adjusting visitor fields descriptions

--- spec/components/schemas/message/visitor.ts
+++ spec/components/schemas/message/visitor.ts
@@ -3,22 +3,27 @@ import { createComponentRef } from '../../../../utils/ref';
 
 const visitor: SchemaObject = {
   title: 'Visitor Object',
-  description: 'It indicates the name of the visitor who sent the message',
+  description: `It provides information about the contact who sent the message.
+                <br>The availability of this information depends on privacy settings of the contact.
+                <br><br>*Only applicable to [WhatsApp](#tag/WhatsApp), [Instagram](#tag/Instagram),
+                [Facebook](#tag/Facebook) and [Telegram](#tag/Telegram) channels.*`,
   type: 'object',
   properties: {
     name: {
       title: 'Name',
-      description: 'Full name of the visitor',
+      description: 'Full name of the contact.',
       type: 'string',
     },
     firstName: {
       title: 'First name',
-      description: 'First name of the visitor',
+      description: `First name of the contact.
+                    <br>*Only applicable to [WhatsApp](#tag/WhatsApp), [Facebook](#tag/Facebook) and [Telegram](#tag/Telegram) channels.*`,
       type: 'string',
     },
     lastName: {
       title: 'Last name',
-      description: 'Last name of the visitor',
+      description: `Last name of the contact.
+                    <br>*Only applicable to [WhatsApp](#tag/WhatsApp), [Facebook](#tag/Facebook) and [Telegram](#tag/Telegram) channels.*`,
       type: 'string',
     },
     userName: {
