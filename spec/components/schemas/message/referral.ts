a0e4b6202eac03a3a9b22db01a3a600b6d44773f
Rafael Souza <rafael@rafael.org>
2021-08-02 20:03:06 -0300

    Adding referral souce type message

--- spec/components/schemas/message/referral.ts
+++ spec/components/schemas/message/referral.ts
@@ -5,8 +5,9 @@ const referral: SchemaObject = {
   title: 'Referral Object',
   description: `Included in notifications where it is possible to track the source of the message. Examples:
 * When an user clicks on an ad that redirects to WhatsApp and sends a message to the business.
-* When an user tags the business on their story.
-* When an user replies to a story from the business.
+* When an user tags the business on their Instagram story.
+* When an user replies to a Instagram story from the business.
+* When an user quotes an message in the Instagram direct conversation.
 
 *Only applicable to [WhatsApp](#tag/WhatsApp) and [Instagram](#tag/Instagram) channels.*`,
   type: 'object',
@@ -37,7 +38,7 @@ const referral: SchemaObject = {
           title: 'Type',
           description: 'The type of content where the message originated.',
           type: 'string',
-          enum: ['ad', 'post', 'story_mention', 'story'],
+          enum: ['ad', 'post', 'story_mention', 'story', 'message'],
         },
         url: {
           title: 'Url',
