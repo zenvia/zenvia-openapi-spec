405ce93cdfc2bf19dcaebd251e81c90e2611ffd7
rafaelgpimenta-zenvia <rafael.pimenta@zenvia.com>
2021-10-01 17:54:56 -0300

    add batch request sample

---
+++ spec/code_samples/batch-messages/index.ts
@@ -0,0 +1,12 @@
+import { ICodeSamples } from '../../../utils/code-samples';
+
+const samples: ICodeSamples = {
+  path: '/message-batches',
+  operations: {
+    post: [
+      { lang: 'Shell', label: 'Bash', source: 'sample.sh' },
+    ],
+  },
+};
+
+export default samples;
