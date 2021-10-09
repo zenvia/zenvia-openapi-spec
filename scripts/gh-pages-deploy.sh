0ed57ac3a41011db96e30a5770e17f29b3545237
Rodrigo Kamada <rodrigo@kamada.com.br>
2021-10-08 22:05:17 -0300

    Removing the Travis CI settings.

--- scripts/gh-pages-deploy.sh
+++
@@ -1,6 +0,0 @@
-git clone --branch gh-pages "https://${GH_TOKEN}@github.com/zenvia/zenvia-openapi-spec.git"  gh-pages
-cp -RT web_deploy gh-pages
-cd gh-pages
-git add .
-git commit -m'Deploy'
-git push "https://${GH_TOKEN}@github.com/zenvia/zenvia-openapi-spec.git"
