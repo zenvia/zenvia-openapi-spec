b0435238d813a3bca28d882ed3def69e07a1aee2
Rafael Souza <rafael@rafael.org>
2021-11-23 14:46:46 -0300

    Improving buttons and quick replies texts

--- spec/components/schemas/content/bases/quick-reply-list.ts
+++ spec/components/schemas/content/bases/quick-reply-list.ts
@@ -6,9 +6,9 @@ export const quickReplyList: SchemaObject = {
   type: 'array',
   title: 'Quick reply buttons',
   description: `Horizontal list of buttons displayed after the content. These *will* fade away once one is clicked.<br>
-    <br>*RCS: A maximum of 11 buttons is supported.*
-    <br>*Instagram: Only text buttons are supported.*
-    <br>*Facebook: Only text and dial buttons ares supported.*`,
+    <br>*__RCS__: A maximum of 11 buttons is supported.*
+    <br>*__Instagram__: Only \`text\` buttons are supported.*
+    <br>*__Facebook__: Only \`text\` and \`dial\` buttons are supported.*`,
   maxItems: 13,
   items: {
     $ref: buttonsRef,
