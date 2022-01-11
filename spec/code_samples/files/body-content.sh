a153dfb92aabe4498fd599204aa1819402bb96f6
Rafael Souza <rafael@rafael.org>
2022-01-11 02:55:27 -0300

    Fixing binary example

--- spec/code_samples/files/body-content.sh
+++ spec/code_samples/files/body-content.sh
@@ -2,4 +2,4 @@ curl 'https://api.zenvia.com/v2/files' \
 -H 'X-API-TOKEN: YOUR_API_TOKEN' \
 -H 'Content-Type: image/png' \
 -H 'Content-Disposition: inline;filename=my-image.png' \
--d '@./image-source.png'
+--data-binary '@./image-source.png'
