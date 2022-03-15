48f3f4780098c5ed80d8661bba5e52b7b6767d55
Alan Naidon <alan.naidon@zenvia.com>
2022-03-15 17:10:34 -0300

    Correcting limit default value

--- spec/components/parameters/files/limit.ts
+++ spec/components/parameters/files/limit.ts
@@ -4,10 +4,10 @@ import { createComponentRef } from '../../../../utils/ref';
 const limit: ParameterObject = {
   name: 'limit',
   in: 'query',
-  default: 1000,
   description: 'The maximum amount of files to be returned. (Min: 1 | Max: 10000)',
   schema: {
     type: 'integer',
+    default: 1000,
   },
 };
 
