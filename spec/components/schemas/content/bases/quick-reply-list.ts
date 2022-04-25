dff150b3d7a92d7efd4377c001503a774676b66b
Alan Naidon <alan.naidon@zenvia.com>
2022-04-25 11:42:26 -0300

    Doc. for GBM: QuickReply, Card, Carousel

--- spec/components/schemas/content/bases/quick-reply-list.ts
+++ spec/components/schemas/content/bases/quick-reply-list.ts
@@ -6,7 +6,8 @@ export const quickReplyList: SchemaObject = {
   type: 'array',
   title: 'Quick reply buttons',
   description: `Horizontal list of buttons displayed after the content. These *will* fade away once one is clicked.<br>
-    <br>*[RCS](#tag/RCS): A maximum of 11 buttons is supported.*
+    <br>*[RCS](#tag/RCS): A maximum of 11 buttons are supported.*
+    <br>*[Google Business Messages](#tag/GBM): A maximum of 13 buttons of 25 characters are supported. Types supported: \`dial\`, \`link\`, \`text\`.*
     <br>*[Instagram](#tag/Instagram): Only \`text\` buttons are supported.*
     <br>*[Facebook](#tag/Facebook): Only \`text\` buttons are supported.*`,
   maxItems: 13,
--- spec/components/schemas/content/gbm/all.ts
+++ spec/components/schemas/content/gbm/all.ts
@@ -1,6 +1,9 @@
 import { SchemaObject } from 'openapi3-ts';
 import { ref as textRef } from '../text';
 import { ref as fileRef } from '../file';
+import { ref as cardRef } from '../card'
+import { ref as carouselRef } from '../carousel'
+import {ref as quickReplyRef } from '../replyable-text'
 import { createComponentRef } from '../../../../../utils/ref';
 
 const all: SchemaObject = {
@@ -9,12 +12,21 @@ const all: SchemaObject = {
     $ref: textRef,
   }, {
     $ref: fileRef,
+  }, {
+    $ref: cardRef,
+  }, {
+    $ref: carouselRef,
+  }, {
+    $ref: quickReplyRef,
   }],
   discriminator: {
     propertyName: 'type',
     mapping: {
       text: textRef,
       file: fileRef,
+      card: cardRef,
+      carousel: carouselRef,
+      replyable_text: quickReplyRef,
     },
   },
 };
