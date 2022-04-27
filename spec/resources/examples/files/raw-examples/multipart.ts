2cedee75a3f3e56320cc33b665c608a2fd3964bc
Rafael Souza <rafael@rafael.org>
2022-04-27 15:48:14 -0300

    Adding /r to where it is necessary

--- spec/resources/examples/files/raw-examples/multipart.ts
+++ spec/resources/examples/files/raw-examples/multipart.ts
@@ -2,21 +2,21 @@ import { ExampleObject } from 'openapi3-ts';
 
 const example: ExampleObject = {
   description: 'Content-Type: multipart/form-data; boundary=3bb873df66f5202e15d4b3b9a7eff',
-  value: `--3bb873df66f5202e15d4b3b9a7eff
-Content-Disposition: form-data; name="file"
-Content-Type: application/json
-
+  value: `--3bb873df66f5202e15d4b3b9a7eff\r
+Content-Disposition: form-data; name="file"\r
+Content-Type: application/json\r
+\r
 {
   "mimeType": "image/png",
   "name": "my-image.png"
-}
---3bb873df66f5202e15d4b3b9a7eff
-Content-Disposition: form-data; name="content"; filename="original-name.png"
-Content-Type: image/png
-
-BINARY-CONTENT
---3bb873df66f5202e15d4b3b9a7eff--
-
+}\r
+--3bb873df66f5202e15d4b3b9a7eff\r
+Content-Disposition: form-data; name="content"; filename="original-name.png"\r
+Content-Type: image/png\r
+\r
+BINARY-CONTENT\r
+--3bb873df66f5202e15d4b3b9a7eff--\r
+\r
 `,
 };
 
