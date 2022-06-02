7dfdaa3d9e3890a49e8ebc39957c9da42fef4b04
Felipe Dal Carobo <felipe.borges@zenvia.com>
2022-06-02 15:09:24 -0300

    product list order

---
+++ spec/components/schemas/content/whatsapp/product-list-order.ts
@@ -0,0 +1,61 @@
+import { SchemaObject } from 'openapi3-ts';
+import { ref as baseRef } from '../base';
+import { createComponentRef } from '../../../../../utils/ref';
+
+const productListOrder: SchemaObject = {
+  type: 'object',
+  allOf: [{
+    $ref: baseRef,
+  }, {
+    type: 'object',
+    properties: {
+      type: {
+        type: 'string',
+        example: 'product-list-order',
+      },
+      text: {
+        type: 'string',
+        description: 'Text content',
+        example: 'Text content',
+      },
+      catalogId: {
+        type: 'string',
+        description: 'The catalog identifier.',
+        example: 'c2c3e42d-6fb7-4ad6-897a-dd7613469f58',
+      },
+      productItems: {
+        type: 'array',
+        description: 'The available fields to be used in this product list order.',
+        items: {
+          type: 'object',
+          properties: {
+            productId: {
+              type: 'string',
+              description: 'The product identifier.',
+            },
+            quantity: {
+              type: 'string',
+              description: 'quantity',
+            },
+            itemPrice: {
+              type: 'string',
+              description: 'itemPrice',
+            },
+            currency: {
+              type: 'string',
+              description: 'currency',
+            },
+          },
+        },
+      },
+    },
+    required: [
+      'type',
+      'catalogId',
+      'productItems',
+    ],
+  }],
+};
+
+export const ref = createComponentRef(__filename);
+export default productListOrder;
--- spec/components/schemas/content/whatsapp/product-list.ts
+++ spec/components/schemas/content/whatsapp/product-list.ts
@@ -11,7 +11,7 @@ const productList: SchemaObject = {
     properties: {
       type: {
         type: 'string',
-        example: 'product_list',
+        example: 'product-list',
       },
       header: {
         type: 'string',
@@ -30,7 +30,7 @@ const productList: SchemaObject = {
       },
       catalogId: {
         type: 'string',
-        description: 'The catalog identifier. Click ...',
+        description: 'The catalog identifier. Click <a href="https://business.facebook.com/commerce" target="_blank">here</a> to go to the catalogs page.',
         example: 'c2c3e42d-6fb7-4ad6-897a-dd7613469f58',
       },
       sections: {
