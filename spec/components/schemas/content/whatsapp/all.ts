d1c6814bb1ee3112cc705cf003ca3e3578891aa9
Felipe Dal Carobo <felipe.borges@zenvia.com>
2022-06-14 10:54:02 -0300

    rename

--- spec/components/schemas/content/whatsapp/all.ts
+++ spec/components/schemas/content/whatsapp/all.ts
@@ -2,9 +2,9 @@ import * as _ from 'lodash';
 import { SchemaObject } from 'openapi3-ts';
 import { createComponentRef } from '../../../../../utils/ref';
 import { ref as productListOrderRef } from './product-list-order';
-import mt from './mt';
+import productListMt from './product-list-mt';
 
-const all: SchemaObject = _.cloneDeep(mt);
+const all: SchemaObject = _.cloneDeep(productListMt);
 all.oneOf.push({ $ref: productListOrderRef });
 all.discriminator.mapping.product_list_order = productListOrderRef;
 
