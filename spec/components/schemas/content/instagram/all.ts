c4987b89295c0e89dd406ab74ecfd8c7eff13825
Rafael Souza <rafael@rafael.org>
2022-07-06 15:44:39 -0300

    Adding card and carousel to the supported instagram content types

--- spec/components/schemas/content/instagram/all.ts
+++ spec/components/schemas/content/instagram/all.ts
@@ -2,6 +2,8 @@ import { SchemaObject } from 'openapi3-ts';
 import { ref as textRef } from '../text';
 import { ref as fileRef } from '../file';
 import { ref as quickRepliesRef } from '../replyable-text';
+import { ref as cardRef } from '../card';
+import { ref as carouselRef } from '../carousel';
 import { createComponentRef } from '../../../../../utils/ref';
 
 const all: SchemaObject = {
@@ -19,6 +21,8 @@ const all: SchemaObject = {
       text: textRef,
       file: fileRef,
       replyable_text: quickRepliesRef,
+      card: cardRef,
+      carousel: carouselRef,
     },
   },
 };
