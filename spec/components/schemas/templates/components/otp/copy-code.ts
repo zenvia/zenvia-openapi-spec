985cc2d67ff3d19de936fdd094ef2902e92eb45b
rafaelgpimenta-zenvia <rafael.pimenta@zenvia.com>
2023-05-29 12:10:44 -0300

    validation

--- spec/components/schemas/templates/components/otp/copy-code.ts
+++ spec/components/schemas/templates/components/otp/copy-code.ts
@@ -7,26 +7,6 @@ const otp: SchemaObject = {
   type: 'object',
   allOf: [{
     $ref: baseRef,
-  }, {
-    type: 'object',
-    properties: {
-      copyButtonText: {
-        description: 'Note that even if your template is using a one-tap autofill button, this value must still be supplied. If we are unable to validate your handshake the authentication template message will display a copy code button with this text instead. Maximum 25 characters.',
-        type: 'string',
-      },
-      addSecurityRecommendation: {
-        description: 'Set to true if you want the template to include the string: "For your security, do not share this code". Set to false to exclude the string.',
-        type: 'boolean',
-      },
-      codeExpirationMinutes: {
-        description: 'Indicates number of minutes the password or code is valid. If omitted, the code expiration warning will not be displayed in the delivered message. You can use a number between 1-90',
-        type: 'number',
-      },
-    },
-    required: [
-      'type',
-      'copyButtonText',
-    ],
   }],
 };
 
