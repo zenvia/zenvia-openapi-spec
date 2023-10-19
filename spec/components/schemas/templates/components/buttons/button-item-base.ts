1cadccad4e7291f8bd7f1f1a7e891cf874a413fd
Guilherme <guilherme.stein@zenvia.com>
2023-10-19 16:26:54 -0300

    feat: adicao de botao mpm e teste de caso

--- spec/components/schemas/templates/components/buttons/button-item-base.ts
+++ spec/components/schemas/templates/components/buttons/button-item-base.ts
@@ -7,7 +7,7 @@ const buttonItemBase: SchemaObject = {
     type: {
       title: 'Button type',
       enum: [
-        'URL', 'PHONE_NUMBER', 'QUICK_REPLY', 'OPT_OUT',
+        'URL', 'PHONE_NUMBER', 'QUICK_REPLY', 'OPT_OUT', 'MPM',
       ],
       type: 'string',
     },
---
+++ spec/components/schemas/templates/components/buttons/button-item-mpm.ts
@@ -0,0 +1,27 @@
+// tslint:disable:max-line-length
+import { SchemaObject } from 'openapi3-ts';
+import { createComponentRef } from '../../../../../../utils/ref';
+import { ref as baseRef } from './button-item-base';
+
+const mpmButton: SchemaObject = {
+  type: 'object',
+  allOf: [{
+    $ref: baseRef,
+  }, {
+    type: 'object',
+    properties: {
+      text: {
+        title: 'Phone number of button',
+        description: 'This field is static with the value of "view items" with automatic translation',
+        type: 'string',
+      },
+    },
+    required: [
+      'type',
+      'text',
+    ],
+  }],
+};
+
+export const ref = createComponentRef(__filename);
+export default mpmButton;
