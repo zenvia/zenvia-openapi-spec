4ae4ab57e54aca5dd47594a0019691508834f956
Isabela <isabela.nascimento@zenvia.com>
2024-08-08 16:43:10 -0300

    lint

--- spec/components/schemas/content/bases/file.ts
+++ spec/components/schemas/content/bases/file.ts
@@ -1,51 +1,51 @@
-import { SchemaObject } from "openapi3-ts";
-import { createComponentRef } from "../../../../../utils/ref";
+import { SchemaObject } from 'openapi3-ts';
+import { createComponentRef } from '../../../../../utils/ref';
 
 const sharedInfo = {
-  mediaTag: "[RCS](#tag/RCS)",
-  mimeTypeDescription: "MIME type of the file to be sent. If not sent, the MIME type will be identified using the content type sent in the request header.",
-  urlDownloadInfo: 'The image at this URL should be publicly accessible and downloadable.'
+  mediaTag: '[RCS](#tag/RCS)',
+  mimeTypeDescription: 'MIME type of the file to be sent. If not sent, the MIME type will be identified using the content type sent in the request header.',
+  urlDownloadInfo: 'The image at this URL should be publicly accessible and downloadable.',
 };
 
 const fileBase: SchemaObject = {
-  type: "object",
-  title: "File",
+  type: 'object',
+  title: 'File',
   properties: {
     fileUrl: {
-      type: "string",
+      type: 'string',
       description: `URL of the file to be sent. ${sharedInfo.urlDownloadInfo}`,
-      example: "https://zenvia.com/document.pdf",
+      example: 'https://zenvia.com/document.pdf',
     },
     thumbnailUrl: {
-      type: "string",
+      type: 'string',
       description: `Image URL to be used as a thumbnail for **media** files. Accepted formats are PNG, JPEG and GIF. ${sharedInfo.urlDownloadInfo}.
                       <br> Only applicable to *${sharedInfo.mediaTag}*`,
-      example: "https://storage.com/image.png",
+      example: 'https://storage.com/image.png',
     },
     thumbnailMimeType: {
-      type: "string",
+      type: 'string',
       description: `${sharedInfo.mimeTypeDescription}<br>
                       <br> Only applicable to *${sharedInfo.mediaTag}*`,
-      example: "image/png",
+      example: 'image/png',
     },
     fileMimeType: {
-      type: "string",
+      type: 'string',
       description: `${sharedInfo.mimeTypeDescription}<br>
                     <br>*[WhatsApp](#tag/WhatsApp): see [supported contents and sizes.](#section/WhatsApp-limitations)*
                     <br>*[Instagram](#tag/Instagram): see [supported contents and sizes.](#section/Instagram-limitations)*
                     <br>*[Telegram](#tag/Telegram): see [supported contents and sizes.](#section/Telegram-limitations)*
                     <br>*[E-Mail](#tag/E-Mail): see [supported contents and sizes.](#section/E-Mail-limitations)*
                     <br>*For the rest of the channels, the limitations are currently undocumented.*`,
-      example: "application/pdf",
+      example: 'application/pdf',
     },
     fileName: {
-      type: "string",
+      type: 'string',
       description: `The name of the file.
                     <br>*Only applicable to [WhatsApp](#tag/WhatsApp), [Facebook](#tag/Facebook), [Telegram](#tag/Telegram), [Google Business Messages](#tag/Google-Business-Messages) and [E-mail](#tag/E-Mail) documents.*`,
-      example: "document.pdf",
+      example: 'document.pdf',
     },
   },
-  required: ["fileUrl"],
+  required: ['fileUrl'],
 };
 
 export const ref = createComponentRef(__filename);
