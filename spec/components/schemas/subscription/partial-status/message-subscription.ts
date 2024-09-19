2e8b7fe520b6da2be6ba7e3c2c920822f957daa5
Davi Machado <davi.machado@zenvia.com>
2024-09-19 14:50:38 -0300

    adding new schemas

---
+++ spec/components/schemas/subscription/partial-status/message-subscription.ts
@@ -0,0 +1,38 @@
+// tslint:disable:max-line-length
+import { SchemaObject } from 'openapi3-ts';
+import { ref as baseRef } from './base';
+import { ref as channelRef } from '../../message/channel';
+import { createComponentRef } from '../../../../../utils/ref';
+
+const subscription: SchemaObject = {
+  type: 'object',
+  allOf: [{
+    $ref: baseRef,
+  }, {
+    type: 'object',
+    properties: {
+      criteria: {
+        type: 'object',
+        properties: {
+          channel: {
+            $ref: channelRef,
+          },
+          direction: {
+            title: 'Message direction',
+            description: 'Indicates whether the message is received from a channel (IN) or sent to a channel (OUT)',
+            type: 'string',
+            enum: [
+              'IN',
+              'OUT',
+            ],
+          },
+        },
+        required: ['channel'],
+      },
+    },
+    required: ['criteria'],
+  }],
+};
+
+export const ref = createComponentRef(__filename);
+export default subscription;
