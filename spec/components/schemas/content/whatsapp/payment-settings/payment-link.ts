250deab91dc768cfbd53b74ff350c2b00a97b551
Alan Naidon <alan.naidon@zenvia.com>
2025-02-11 14:29:52 -0300

    WhatsApp Payment - Link and Boleto

---
+++ spec/components/schemas/content/whatsapp/payment-settings/payment-link.ts
@@ -0,0 +1,22 @@
+import { SchemaObject } from 'openapi3-ts';
+import { createComponentRef } from '../../../../../../utils/ref';
+
+const paymentLink: SchemaObject = {
+  type: 'object',
+  title: 'payment_link',
+  properties: {
+    type: {
+      type: 'string',
+      description: 'The type of the payment method.',
+      enum: ['payment_link'],
+    },
+    paymentLink: {
+      type: 'string',
+      description: 'A valid payment URL.',
+    },
+  },
+  required: ['type', 'paymentLink'],
+}
+
+export const ref = createComponentRef(__filename);
+export default paymentLink;
