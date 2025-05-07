df6cc972a88d88d781adb6c225f625b7da2d50a4
Felipe Silvano Perini <felipe.perini@zenvia.com>
2025-05-07 08:06:40 -0300

    feat: implements get ticket routes

---
+++ spec/components/schemas/support-service/ticket-status-histories.ts
@@ -0,0 +1,45 @@
+import { SchemaObject } from 'openapi3-ts';
+import { createComponentRef } from '../../../../utils/ref';
+import { ref as justificationRef } from './context/justification';
+import { ref as statusRef } from './context/status';
+
+const base: SchemaObject = {
+  title: 'Ticket Status History',
+  type: 'object',
+  properties: {
+    status: {
+      $ref: statusRef,
+    },
+    justification: {
+      $ref: justificationRef,
+    },
+    permanencyTimeFullTime: {
+      title: 'Permanency Full Time',
+      description: 'Duration of the status in calendar hours. Returns the total time in seconds.',
+      type: 'number',
+      readOnly: true,
+    },
+    permanencyTimeWorkingTime: {
+      title: 'Permanency Working Time',
+      description: 'Duration of the status in business hours. Returns the total time in seconds.',
+      type: 'number',
+      readOnly: true,
+    },
+    updatedById: {
+      title: 'Updated by identification',
+      description: 'User that changed the ticket status.',
+      type: 'string',
+      nullable: true,
+      readOnly: true,
+    },
+    updatedAt: {
+      description: 'Timestamp of the update.',
+      type: 'string',
+      example: '2022-05-23T19:37:59.000Z',
+      readOnly: true,
+    },
+  },
+};
+
+export const ref = createComponentRef(__filename);
+export default base;
