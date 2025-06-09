191f7dd5009fb37655e1d5d449eeff19c6602a65
beatriz.assumcao <beatriz.assumcao@zenvia.com>
2025-06-09 13:58:28 -0300

    fix: content scope

--- spec/components/schemas/content/whatsapp/all.ts
+++ spec/components/schemas/content/whatsapp/all.ts
@@ -2,10 +2,11 @@ import * as _ from 'lodash';
 import { SchemaObject } from 'openapi3-ts';
 import { createComponentRef } from '../../../../../utils/ref';
 import { ref as productListOrderRef } from './product-list-order';
+import { ref as unsupportedRef } from './unsupported';
 import mt from './mt';
 
 const all: SchemaObject = _.cloneDeep(mt);
-all.oneOf.push({ $ref: productListOrderRef });
+all.oneOf.push({ $ref: productListOrderRef }, { $ref: unsupportedRef });
 all.discriminator.mapping.product_list_order = productListOrderRef;
 
 export const ref = createComponentRef(__filename);
