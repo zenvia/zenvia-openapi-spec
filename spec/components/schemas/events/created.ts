3a362c7ed12a53a2180204b5d625444f7b6afb5a
Vinícius Vieira <vinicius.v.meta@terceiros.zenvia.com>
2025-07-08 14:48:50 -0300

    new conversion event subscription

---
+++ spec/components/schemas/events/created.ts
@@ -0,0 +1,25 @@
+import { SchemaObject } from 'openapi3-ts';
+import { createComponentRef } from '../../../../utils/ref';
+import { ref as baseConversationRef } from './conversation-base';
+
+const created: SchemaObject = {
+  type: 'object',
+  properties: {
+    id: { type: 'string' },
+    timestamp: { type: 'string', format: 'date-time' },
+    type: {
+      type: 'string',
+      enum: ['CONVERSATION_CREATED', 'CONVERSATION_QUEUED', 'CONVERSATION_STARTED', 'CONVERSATION_UNSNOOZED']
+    },
+    subscriptionId: { type: 'string' },
+    conversation: {
+      allOf: [
+        { $ref: baseConversationRef }
+      ],
+      required: ['id', 'status', 'statusTimestamp']
+    }
+  }
+};
+
+export const ref = createComponentRef(__filename);
+export default created;
---
+++ spec/components/schemas/events/transferred.ts
@@ -0,0 +1,40 @@
+import { SchemaObject } from 'openapi3-ts';
+import { createComponentRef } from '../../../../utils/ref';
+import { ref as baseConversationRef } from './conversation-base';
+
+const transferred: SchemaObject = {
+  type: 'object',
+  properties: {
+    id: { type: 'string' },
+    timestamp: { type: 'string', format: 'date-time' },
+    type: { type: 'string', enum: ['CONVERSATION_TRANSFERRED'] },
+    subscriptionId: { type: 'string' },
+    conversation: {
+      allOf: [
+        { $ref: baseConversationRef },
+        {
+          type: 'object',
+          properties: {
+            transferType: { type: 'string' },
+            transferredBy: {
+              type: 'object',
+              properties: {
+                id: { type: 'string' },
+                type: { type: 'string' }
+              }
+            },
+            newUserId: { type: 'string' },
+            newGroupId: { type: 'string' },
+            previousUserId: { type: 'string' },
+            previousGroupId: { type: 'string' }
+          },
+          required: ['transferType']
+        }
+      ],
+      required: ['id', 'status', 'statusTimestamp']
+    }
+  }
+};
+
+export const ref = createComponentRef(__filename);
+export default transferred;
