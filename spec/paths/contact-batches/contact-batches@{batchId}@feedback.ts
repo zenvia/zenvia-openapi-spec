f8a33e6fe7a2992833f7325911bbb182ac586ffa
Isaac Hermel <isaac.reginato@zenvia.com>
2025-07-30 17:17:37 -0300

    feat: add Contact Batch API documentation

---
+++ spec/paths/contact-batches/contact-batches@{batchId}@feedback.ts
@@ -0,0 +1,37 @@
+import {
+  OperationObject,
+  PathItemObject,
+  ResponseObject,
+  ResponsesObject,
+  SchemaObject,
+} from 'openapi3-ts';
+import { ref as errorResponseRef } from '../../components/responses/error';
+
+const get: OperationObject = {
+  summary: 'Retrieve batch feedback by id',
+  description: 'Retrieve a contact batch feedback by its id as a CSV file.',
+  tags: ['Contact Batches'],
+  responses: {
+    200: {
+      description: 'Batch feedback retrieved successfully in CSV format.',
+      content: {
+        'text/csv': {
+          schema: {
+            type: 'string',
+            format: 'binary',
+            description: 'A CSV file with contacts feedback data, using semicolons as delimiters. This CSV will contain two new columns, status (the processing status of each line) and errorMessages (the validation errors for each line).',
+          } as SchemaObject,
+        },
+      },
+    } as ResponseObject,
+    default: {
+      $ref: errorResponseRef,
+    },
+  } as ResponsesObject,
+};
+
+const path: PathItemObject = {
+  get,
+};
+
+export default path;
--- spec/tags/index.ts
+++ spec/tags/index.ts
@@ -1,87 +1,36 @@
 import { TagObject } from 'openapi3-ts';
 import { rawLoad } from '../../utils/raw-load';
 
-const contentTypesDescription = rawLoad(__dirname, './content-types.md');
-const smsDescription = rawLoad(__dirname, './sms.md');
-const whatsappDescription = rawLoad(__dirname, './whatsapp.md');
-const facebookDescription = rawLoad(__dirname, './facebook.md');
-const batchesDescription = rawLoad(__dirname, './batches.md');
-const flowBatchesDescription = rawLoad(__dirname, './flow-batches.md');
-const subscriptionsDescription = rawLoad(__dirname, './subscriptions.md');
-const templatesDescription = rawLoad(__dirname, './templates.md');
-const messageReportDescription = rawLoad(__dirname, './message-report.md');
-const flowReportDescription = rawLoad(__dirname, './flow-report.md');
-const rcsDescription = rawLoad(__dirname, './rcs.md');
-const telegramDescription = rawLoad(__dirname, './telegram.md');
-const voiceDescription = rawLoad(__dirname, './voice.md');
-const gbmDescription = rawLoad(__dirname, './gbm.md');
-const instagramDescription = rawLoad(__dirname, './instagram.md');
-const emailDescription = rawLoad(__dirname, './email.md');
-const filesDescription = rawLoad(__dirname, './files.md');
-const twoFactorAuthentication = rawLoad(__dirname, './2fa.md');
-const marketingAutomationDescription = rawLoad(__dirname, './marketing-automations.md');
-const statusGroupsDescription = rawLoad(__dirname, './status-groups.md');
+const tagDefinitions: {
+  name: string;
+  file: string;
+}[] = [
+  { name: 'Content types', file: 'content-types' },
+  { name: 'SMS', file: 'sms' },
+  { name: 'WhatsApp', file: 'whatsapp' },
+  { name: 'Facebook', file: 'facebook' },
+  { name: 'Instagram', file: 'instagram' },
+  { name: 'Google Business Messages', file: 'gbm' },
+  { name: 'RCS', file: 'rcs' },
+  { name: 'Telegram', file: 'telegram' },
+  { name: 'Voice', file: 'voice' },
+  { name: 'E-Mail', file: 'email' },
+  { name: 'Flow Batches', file: 'flow-batches' },
+  { name: 'Message Batches', file: 'batches' },
+  { name: 'Two Factor Authentication', file: '2fa' },
+  { name: 'Webhooks', file: 'subscriptions' },
+  { name: 'Templates', file: 'templates' },
+  { name: 'Message Report', file: 'message-report' },
+  { name: 'Flow Report', file: 'flow-report' },
+  { name: 'Files', file: 'files' },
+  { name: 'Marketing Automations', file: 'marketing-automations' },
+  { name: 'Status Groups', file: 'status-groups' },
+  { name: 'Contact Batches', file: 'contact-batches' },
+];
 
-const tags: TagObject[] = [{
-  name: 'Content types',
-  description: contentTypesDescription,
-}, {
-  name: 'SMS',
-  description: smsDescription,
-}, {
-  name: 'WhatsApp',
-  description: whatsappDescription,
-}, {
-  name: 'Facebook',
-  description: facebookDescription,
-}, {
-  name: 'Instagram',
-  description: instagramDescription,
-}, {
-  name: 'Google Business Messages',
-  description: gbmDescription,
-}, {
-  name: 'RCS',
-  description: rcsDescription,
-}, {
-  name: 'Telegram',
-  description: telegramDescription,
-}, {
-  name: 'Voice',
-  description: voiceDescription,
-}, {
-  name: 'E-Mail',
-  description: emailDescription,
-}, {
-  name: 'Flow Batches',
-  description: flowBatchesDescription,
-}, {
-  name: 'Message Batches',
-  description: batchesDescription,
-}, {
-  name: 'Two Factor Authentication',
-  description: twoFactorAuthentication,
-}, {
-  name: 'Webhooks',
-  description: subscriptionsDescription,
-}, {
-  name: 'Templates',
-  description: templatesDescription,
-}, {
-  name: 'Message Report',
-  description: messageReportDescription,
-}, {
-  name: 'Flow Report',
-  description: flowReportDescription,
-}, {
-  name: 'Files',
-  description: filesDescription,
-}, {
-  name: 'Marketing Automations',
-  description: marketingAutomationDescription,
-},{
-  name: 'Status Groups',
-  description: statusGroupsDescription,
-}];
+const tags: TagObject[] = tagDefinitions.map(tag  => ({
+  name: tag.name,
+  description: rawLoad(__dirname, `./${tag.file}.md`),
+}));
 
 export default tags;
