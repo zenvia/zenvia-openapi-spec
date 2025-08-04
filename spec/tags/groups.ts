ed796953df032119086c5746b061b4fd542216c5
Isaac Hermel <isaac.reginato@zenvia.com>
2025-08-04 14:27:13 -0300

    feat: update groups

--- spec/tags/groups.ts
+++ spec/tags/groups.ts
@@ -1,82 +1,80 @@
 import { TagGroupObject } from 'openapi3-ts';
 
-const groups: TagGroupObject[] = [{
-  name: 'Customer Data Platform',
-  tags: [
-    'Contact Batches',
-    'Product Batches',
-    'Order Batches',
-    'Invoice Batches',
-  ],
-},{
-  name: 'Messaging',
-  tags: [
-    'Content types',
-    'SMS',
-    'WhatsApp',
-    'Instagram',
-    'Facebook',
-    'RCS',
-    'Telegram',
-    'Google Business Messages',
-    'Voice',
-    'E-Mail',
-    'Message Batches',
-    'Two Factor Authentication',
-  ],
-}, {
-  name: 'Webhooks',
-  tags: [
-    'Webhooks',
-  ],
-}, {
-  name: 'Templates',
-  tags: [
-    'Templates',
-  ],
-}, {
-  name: 'File Storage',
-  tags: [
-    'Files',
-  ],
-}, {
-  name: 'Flow',
-  tags: [
-    'Flow Batches',
-  ],
-}, {
-  name: 'Chatbot',
-  tags: [
-    'Chatbot Outbound',
-  ],
-}, {
-  name: 'Contacts',
-  tags: [
-    'Contacts',
-    'Contact Lists',
-    'Contact Custom Data Fields',
-  ],
-},{
-  name: 'Reports',
-  tags: [
-    'Message Report',
-    'Flow Report',
-  ],
-}, {
-  name: 'Marketing Automations',
-  tags: [
-    'Marketing Automations',
-  ],
-}, {
-  name: 'Support Service',
-  tags: [
-    'Tickets',
-  ],
-}, {
-  name: 'Status',
-  tags: [
-    'Status Groups',
-  ],
-}];
+const groups: TagGroupObject[] = [
+  {
+    name: 'Messaging',
+    tags: [
+      'Content types',
+      'SMS',
+      'WhatsApp',
+      'Instagram',
+      'Facebook',
+      'RCS',
+      'Telegram',
+      'Google Business Messages',
+      'Voice',
+      'E-Mail',
+      'Message Batches',
+      'Two Factor Authentication',
+    ],
+  },
+  {
+    name: 'Webhooks',
+    tags: ['Webhooks'],
+  },
+  {
+    name: 'Templates',
+    tags: ['Templates'],
+  },
+  {
+    name: 'File Storage',
+    tags: ['Files'],
+  },
+  {
+    name: 'Flow',
+    tags: ['Flow Batches'],
+  },
+  {
+    name: 'Chatbot',
+    tags: ['Chatbot Outbound'],
+  },
+  {
+    name: 'Contacts',
+    tags: [
+      'Contacts',
+      'Contact Lists',
+      'Contact Custom Data Fields',
+      'Contact Batches',
+    ],
+  },
+  {
+    name: 'Products',
+    tags: ['Product Batches'],
+  },
+  {
+    name: 'Orders',
+    tags: ['Order Batches'],
+  },
+  {
+    name: 'Invoices',
+    tags: ['Invoice Batches'],
+  },
+  {
+    name: 'Reports',
+    tags: ['Message Report', 'Flow Report'],
+  },
+  {
+    name: 'Marketing Automations',
+    tags: ['Marketing Automations'],
+  },
+  {
+    name: 'Support Service',
+    tags: ['Tickets'],
+  },
+  {
+    name: 'Status',
+    tags: ['Status Groups'],
+  },
+];
 
 export default groups;
