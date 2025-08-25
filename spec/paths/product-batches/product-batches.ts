b253f48c901083c02ff69b989b945702a9b481f5
Isaac Hermel <isaac.reginato@zenvia.com>
2025-08-25 11:41:36 -0300

    fix(partners-apis): add minLength prop to ensure its mandatory

--- spec/paths/product-batches/product-batches.ts
+++ spec/paths/product-batches/product-batches.ts
@@ -29,6 +29,7 @@ const post: OperationObject = {
               type: 'string',
               format: 'binary',
               description: 'CSV file to be imported.',
+              minLength: 1,
             },
           },
           required: [
