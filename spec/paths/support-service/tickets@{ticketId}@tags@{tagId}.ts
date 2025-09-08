73f62368262e4f9e96ecd4c8eacfcb93679d29c8
felipe.perini <felipe.perini@zenvia.com>
2025-09-08 12:51:15 -0300

    feat: remove body from delete

--- spec/paths/support-service/tickets@{ticketId}@tags@{tagId}.ts
+++ spec/paths/support-service/tickets@{ticketId}@tags@{tagId}.ts
@@ -2,22 +2,11 @@ import { PathItemObject, OperationObject, ResponseObject, ResponsesObject } from
 import { ref as errorResponseRef } from '../../components/responses/error';
 import { ref as ticketIdRef } from '../../components/parameters/support-service/ticketId';
 import { ref as tagIdRef } from '../../components/parameters/support-service/tagId';
-import { ref as ticketTagsDeleteDataFieldRef } from '../../components/schemas/support-service/ticket-tags-delete';
 
 const del: OperationObject = {
   summary: 'Delete tag from ticket',
   description: 'Allows to delete a tag from ticket.',
   tags: ['Ticket Tags'],
-  requestBody: {
-    required: false,
-    content: {
-      'application/json': {
-        schema: {
-          $ref: ticketTagsDeleteDataFieldRef,
-        },
-      },
-    },
-  },
   responses: {
     204: {
       description: 'No content',
