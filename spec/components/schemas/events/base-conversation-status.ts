0cf0bcf7ab7be796298b9a6cf134c83b7438efce
Lucas Morais <lucas.msantos@zenvia.com>
2026-01-21 10:49:12 -0300

    fix(conversation-events): adjusts types

---
+++ spec/components/schemas/events/base-conversation-status.ts
@@ -0,0 +1,34 @@
+import { SchemaObject } from 'openapi3-ts';
+import { createComponentRef } from '../../../../utils/ref';
+
+const conversationBase: SchemaObject = {
+  type: 'object',
+  properties: {
+    id: {
+      title: 'Event Id',
+      description: 'ID of the webhook event. Can be used to identify retried deliveries in case of webhook errors.',
+      type: 'string',
+    },
+    timestamp: {
+      title: 'Event Timestamp',
+      description: 'Timestamp of the webhook event',
+      type: 'string',
+      format: 'date-time',
+    },
+    type: {
+      title: 'Event type',
+      type: 'string',
+      enum: [
+        'CONVERSATION_STATUS',
+      ],
+    },
+    subscriptionId: {
+      title: 'Webhook ID',
+      description: 'ID of the webhook that caused this event to be delivered',
+      type: 'string',
+    },
+  },
+};
+
+export const ref = createComponentRef(__filename);
+export default conversationBase;
