89d591fb97ab6acdf829ee41c1869e5160836d0e
felipedalcarobo <felipedalcarobo@ymail.com>
2026-04-07 08:40:51 -0300

    Alterar critérios de filtragem

--- spec/components/schemas/events/message-status-event.ts
+++ spec/components/schemas/events/message-status-event.ts
@@ -23,6 +23,22 @@ const messageEvent: SchemaObject = {
         title: 'Message Channel',
         type: 'string',
       },
+      direction: {
+        title: 'Message Status Direction',
+        description: `Indicates whether the message status is received from a channel (**IN**), sent to a channel (**OUT**), or both (**ALL**).
+
+* **IN**: Received messages.
+* **OUT**: Sent messages.
+* **ALL**: Filters messages from both directions.
+
+> **Note:** The **OUT** direction is under construction.`,
+        type: 'string',
+        enum: [
+          'IN',
+          'OUT',
+          'ALL',
+        ],
+      },
       messageId: {
         title: 'Message ID',
         description: 'ID of the message whose status is being delivered',
