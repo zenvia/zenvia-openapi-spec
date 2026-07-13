8b64ca44900c9ef1ce5b47443497cebe01a50ed2
Alan Naidon <alan.naidon@zenvia.com>
2026-07-13 12:07:55 -0300

    Adding text property to button payload

--- spec/components/schemas/templates/components/buttons/button-item-request-contact-info.ts
+++ spec/components/schemas/templates/components/buttons/button-item-request-contact-info.ts
@@ -1,18 +1,27 @@
 import { SchemaObject } from 'openapi3-ts';
 import { createComponentRef } from '../../../../../../utils/ref';
 import { ref as baseRef } from './button-item-base';
+import { ref as btnTextRef } from './button-item-text';
 
 const requestContactInfoButton: SchemaObject = {
   type: 'object',
-  allOf: [{
-    $ref: baseRef,
-  }],
-  properties: {
-    type: {
-      type: 'string',
-      enum: ['REQUEST_CONTACT_INFO'],
+  allOf: [
+    { $ref: baseRef },
+    { $ref: btnTextRef },
+    {
+      type: 'object',
+      properties: {
+        type: {
+          type: 'string',
+          enum: ['REQUEST_CONTACT_INFO'],
+        },
+      },
+      required: [
+        'type',
+        'text',
+      ],
     },
-  },
+  ],
 };
 
 export const ref = createComponentRef(__filename);
