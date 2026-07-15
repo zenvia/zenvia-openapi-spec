0eccb2878c85dcc4635d8b50b1d6e2d758dd603f
Alan Naidon <alan.naidon@zenvia.com>
2026-07-15 11:38:27 -0300

    Updating contac info button property

--- spec/components/schemas/templates/components/buttons/button-item-request-contact-info.ts
+++ spec/components/schemas/templates/components/buttons/button-item-request-contact-info.ts
@@ -4,22 +4,15 @@ import { ref as baseRef } from './button-item-base';
 
 const requestContactInfoButton: SchemaObject = {
   type: 'object',
-  allOf: [
-    { $ref: baseRef },
-    {
-      type: 'object',
-      properties: {
-        type: {
-          type: 'string',
-          enum: ['REQUEST_CONTACT_INFO'],
-        },
-      },
-      required: [
-        'type',
-        'text',
-      ],
+  allOf: [{
+    $ref: baseRef,
+  }],
+  properties: {
+    type: {
+      type: 'string',
+      enum: ['REQUEST_CONTACT_INFO'],
     },
-  ],
+  },
 };
 
 export const ref = createComponentRef(__filename);
